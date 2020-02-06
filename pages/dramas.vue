2467e07630216fe58e5617a8c5cd35017e4e6e58
Emilie <emilie1.bernard@gmail.com>
2020-02-06 18:25:29 +0100

    Logo and drama page

--- pages/dramas.vue
+++ pages/dramas.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id="contact-page" class="page-wrapper contact-page content-page">
+  <div id="dramas-page" class="page-wrapper dramas-page content-page">
     <site-hero
-      title="Contact Us"
-      subtitle="Example Contact Page"
-      image="/uploads/contact-hero.jpg"
+      title="Dramas"
+      subtitle="Tous les dramas vu et mon avi"
+      image="/uploads/drama-hero.jpg"
     ></site-hero>
     <main-section theme="sidebar-right">
       <template v-slot:default>
@@ -11,19 +11,11 @@
           <div class="tile is-parent">
             <article class="tile is-child box">
               <p class="title">
-                Hello!
+                  Coréens
               </p>
               <p class="subtitle">
-                What can I do for you?
+                Ma liste des dramas coréens vu
               </p>
-              <figure class="image is-1by1 ">
-                <opti-image
-                  :src="require('~/assets/uploads/contact-person.jpg').src"
-                  :srcset="
-                    require('~/assets/uploads/contact-person.jpg').srcSet
-                  "
-                />
-              </figure>
               <br />
               <div class="content">
                 <p>
@@ -43,29 +35,56 @@
           </div>
           <div class="tile is-parent is-8">
             <article class="tile is-child box">
-              <contact-form />
+              <p class="title">
+                  Taiwanais
+              </p>
+              <p class="subtitle">
+                Ma liste des dramas taiwanais vu
+              </p>
+              <br />
+              <div class="content">
+                <p>
+                  <strong>
+                    Edit this page in<code>/pages/contact.vue</code>to fit your
+                    needs.
+                  </strong>
+                </p>
+                <p>
+                  This form doesn't actually work but would be easy enough to
+                  slap an action on to have it post wherever you'd like. Or put
+                  the netlify attribute on it and have netlify handle all your
+                  form submissions...
+                </p>
+              </div>
             </article>
           </div>
-        </div>
-      </template>
-      <template v-slot:sidebar>
-        <h3 class="subtitle is-4">
-          Latest Posts
-        </h3>
-        <!-- Latest Posts -->
-        <posts-grid :per-row="1" :number="2" />
+        </div><div class="tile is-ancestor">
+          <div class="tile is-parent">
+            <article class="tile is-child box">
+              <p class="title">
+                  Autres
+              </p>
+              <p class="subtitle">
+                Ma liste des dramas chinois/japonais/thailandais vu
+              </p>
+              <br />
+              <div class="content">
+                <p>
+                  <strong>
+                    Edit this page in<code>/pages/contact.vue</code>to fit your
+                    needs.
+                  </strong>
+                </p>
+                <p>
+                  This form doesn't actually work but would be easy enough to
+                  slap an action on to have it post wherever you'd like. Or put
+                  the netlify attribute on it and have netlify handle all your
+                  form submissions...
+                </p>
+              </div>
+            </article>
+          </div>
       </template>
     </main-section>
   </div>
 </template>
-<script>
-import ContactForm from '~/components/ContactForm'
-export default {
-  head() {
-    return {
-      title: `Contact | ${this.$siteConfig.siteName}`
-    }
-  },
-  components: { ContactForm }
-}
-</script>
