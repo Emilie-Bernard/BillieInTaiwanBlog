138b5f0a55fd5b3bd497aecbf45e597d037295d5
Emilie Bernard <56219642+Emilie-Bernard@users.noreply.github.com>
2020-02-07 15:02:43 +0100

    Create taiwan.vue

---
+++ pages/taiwan.vue
@@ -0,0 +1,230 @@
+<template>
+  <div id="dramas-page" class="page-wrapper dramas-page">
+    <site-hero
+      title="Dramas Taiwanais"
+      subtitle="Pastel et facilitée"
+      image="/uploads/drama-hero.jpg"
+    ></site-hero>
+    <main-section theme="one-column">
+      <template v-slot:default>
+        <div class="tile is-ancestor">
+          <div class="tile is-parent">
+            <article class="tile is-child box">
+              <p class="title">
+                La liste des dramas taiwanais que j'ai eu l'occasion de visionner
+              </p>
+              <p class="subtitle">
+                Petit à petit nous sombrons dans le monde infini des dramas 
+              </p>
+              <div class="content">
+              <p>
+              ⭐ = drama que j'ai adoré
+              <br>
+              😍 = drama que j'ai aimé
+              <br>
+              😄 = drama qui m'ont plus
+              </p>
+                <p>
+                  <strong> 0-9 </strong>
+                  <br>
+                  
+                </p>
+                <p>
+                  <strong> A </strong>
+                  <br> 
+                   <span style="color:#f0ad4e">Alice in Wonder City 😄</span> 
+                   <span style="color:#f0ad4e">Apple In Your Eye</span>
+                   <span style="color:#f0ad4e">Attention, Love! ⭐</span>
+                   <span style="color:#f0ad4e">Autumn's Concerto ⭐</span>
+                </p>
+                <p>
+                  <strong> B </strong>
+                  <br>
+                  <span style="color:#f0ad4e">Back to 1989 ⭐</span>
+                  <span style="color:#f0ad4e">Back to Home ⭐</span>
+                  <span style="color:#f0ad4e">Before We Get Married </span>
+                  <span style="color:#f0ad4e">Behind Your Smile</span>
+                  <span style="color:#f0ad4e">Better Man ⭐</span>
+                  <span style="color:#f0ad4e">Boysitter 😄</span>
+                  <span style="color:#f0ad4e">Bromance ⭐</span>
+                <p>
+                  <strong> C </strong>
+                  <br>
+                  </p>
+                <p>
+                  <strong> D </strong>
+                  <br>
+                  <span style="color:#f0ad4e">Devil Beside You ⭐</span>
+                  <span style="color:#f0ad4e">Down With Love 😍</span>
+                  <span style="color:#f0ad4e">Déjà Vu 😍</span>
+                <p>
+                  <strong> E </strong>
+                  <br>
+                  <span style="color:#f0ad4e">Easy Fortune Happy Life 😄</span> 
+                </p>
+                <p>
+                  <strong> F </strong>
+                  <br>
+                  <span style="color:#f0ad4e">Fabulous Boys</span>
+                  <span style="color:#f0ad4e">Fall in Love With Me ⭐</span>
+                  <span style="color:#f0ad4e">Fated To Love You 😍</span>
+                  <span style="color:#f0ad4e">Five Missions ⭐</span>
+               </p>
+               <p>
+                  <strong> G </strong>
+                  <br>
+                  <span style="color:#f0ad4e">GTO TAIWAN 😍</span>
+               </p>
+               <p>
+                  <strong> H </strong>
+                  <br>
+                  <span style="color:#f0ad4e">Hello Again! 😍</span>
+                  <span style="color:#f0ad4e">Hi My Sweetheart ⭐</span>
+                  <span style="color:#f0ad4e">HIStory 2: Cross The Line 😍</span>
+                  <span style="color:#f0ad4e">HIStory 2: Right or Wrong ⭐</span>
+                  <span style="color:#f0ad4e">HIStory 3: Make Our Days Count ⭐</span>
+                  <span style="color:#f0ad4e">HIStory 3: Trapped ⭐</span>
+                  <span style="color:#f0ad4e">HIStory: My Hero ⭐</span>
+                  <span style="color:#f0ad4e">HIStory: Obsessed ⭐</span>
+                  <span style="color:#f0ad4e">HIStory: Stay Away From Me ⭐</span>
+                  <span style="color:#f0ad4e">Home Sweet Home 😍</span>
+               </p>
+               <p>
+                  <strong> I </strong>
+                  <br>
+                  <span style="color:#f0ad4e">I Am Sorry, I Love You 😍</span>
+                  <span style="color:#f0ad4e">Inborn Pair ⭐</span>
+                  <span style="color:#f0ad4e">In Time With You ⭐</span>
+                  <span style="color:#f0ad4e">Iron Ladies 😍</span>
+                  <span style="color:#f0ad4e">It Started With a Kiss</span>
+               </p>
+               <p>
+                  <strong> J </strong>
+                  <br>
+                  <span style="color:#f0ad4e">Jin Da Hua De Hua Li Mao Xian 😄</span>
+                  <span style="color:#f0ad4e">JOJO's World 😄</span>
+                  <span style="color:#f0ad4e">Just You ⭐</span>
+               </p>
+               <p>
+                  <strong> K </strong>
+                  <br>
+                </p>
+               <p>
+                  <strong> L </strong>
+                  <br>
+                  <span style="color:#f0ad4e">Le Jun Kai 😄</span>
+                  <span style="color:#f0ad4e">Leo 😄</span>
+                  <span style="color:#f0ad4e">Lion Pride 😍</span>
+                  <span style="color:#f0ad4e">Love, Now 😄</span>
+                  <span style="color:#f0ad4e">Love @ Seventeen 😄</span>
+                  <span style="color:#f0ad4e">Love Around 😍</span>
+                  <span style="color:#f0ad4e">Love Buffet</span>
+                  <span style="color:#f0ad4e">Love By Design 😍</span>
+                  <span style="color:#f0ad4e">Love Cheque Charge</span>
+                  <span style="color:#f0ad4e">Love Keeps Going</span>
+                  <span style="color:#f0ad4e">Love of Sandstorm</span>
+                  <span style="color:#f0ad4e">Love Or Bread</span>
+                  <span style="color:#f0ad4e">Love You ⭐</span>
+               </p>
+               <p>
+                  <strong> M </strong>
+                  <br>
+                  <span style="color:#f0ad4e">Marry Me Or Not? ⭐</span>
+                  <span style="color:#f0ad4e">Material Queen 😄</span>
+                  <span style="color:#f0ad4e">Memory Love 😍</span>
+                  <span style="color:#f0ad4e">Meteor Garden ⭐</span>
+                  <span style="color:#f0ad4e">Miss In Kiss ⭐</span>
+                  <span style="color:#f0ad4e">Miss No Good 😍</span>
+                  <span style="color:#f0ad4e">Miss Rose 😄</span>
+                  <span style="color:#f0ad4e">Momo Love 😄</span>
+                  <span style="color:#f0ad4e">Moon River 😄</span>
+                  <span style="color:#f0ad4e">Murphy's Law of Love 😍</span>
+               </p>
+               <p>
+                  <strong> N </strong>
+                  <br>
+               <p>
+                  <strong> O </strong>
+                  <br>
+                  <span style="color:#f0ad4e">Office Girls 😄</span>
+                  <span style="color:#f0ad4e">Once More Heartbeat 😄</span>
+               </p>
+               <p>
+                  <strong> P </strong>
+                  <br>
+                   <span style="color:#f0ad4e">Pleasantly Surprised 😍</span>
+                   <span style="color:#f0ad4e">Prince Of Wolf ⭐</span>
+                   span style="color:#f0ad4e">Prince Turns to Frog</span>
+                   <span style="color:#f0ad4e">Prince William</span>
+               </p>
+               <p>
+                  <strong> Q </strong>
+                  <br>
+               </p>
+               <p>
+                  <strong> R </strong>
+                  <br>
+                  <span style="color:#f0ad4e">Refresh Man 😄</span>
+               </p>
+               <p>
+                  <strong> S </strong>
+                  <br>
+                  <span style="color:#f0ad4e">Say Again Yes I Do 😍</span>
+                  <span style="color:#f0ad4e">See You in Time </span>
+                  <span style="color:#f0ad4e">Single Ladies Senior 😍</span>
+                  <span style="color:#f0ad4e">Skip Beat !</span>
+                  <span style="color:#f0ad4e">Smiling Pasta 😍</span>
+                  <span style="color:#f0ad4e">Someday or One Day ⭐⭐</span>
+                  <span style="color:#f0ad4e">Summer's Desire ⭐</span>
+                  <span style="color:#f0ad4e">Sunshine Angel</span>
+                  <span style="color:#f0ad4e">Swimming Battle 😄</span>
+               </p>
+               <p>
+                  <strong> T </strong>
+                  <br>
+                  <span style="color:#f0ad4e">The Crossing Hero 😍</span> 
+                  <span style="color:#f0ad4e">The Magicians of Love 😄</span>
+                  <span style="color:#f0ad4e">The Masked Lover ⭐</span>
+                  <span style="color:#f0ad4e">The Perfect Match 😄</span>
+                  <span style="color:#f0ad4e">The Pursuit of Happiness 😍</span>
+                  <span style="color:#f0ad4e">The Way We Were 😍</span>
+                  <span style="color:#f0ad4e">They Kiss Again ⭐</span>
+                  <span style="color:#f0ad4e">ToGetHer 😄</span>
+                  <span style="color:#f0ad4e">Tokyo Juliet 😄</span>
+                  <span style="color:#f0ad4e">Triad Princess ⭐</span>
+                  </p>
+               <p>
+                  <strong> V </strong>
+                  <br>
+                  <span style="color:#f0ad4e">V-Focus ⭐</span>
+               </p>
+               <p>
+                  <strong> W </strong>
+                  <br>
+                  <span style="color:#f0ad4e">When I See You Again 😍</span>
+                  <span style="color:#f0ad4e">Who's The One 😄</span> 
+                  <span style="color:#f0ad4e">Why Why Love</span>
+               </p>
+               <p>
+                  <strong> X </strong>
+                  <br>
+                  <span style="color:#f0ad4e">Xiao Hua De Tie Shen Gao Shou</span>
+               </p>
+               <p>
+                  <strong> Y </strong>
+                  <br>
+                  <span style="color:#f0ad4e">Ying Ye 3 + 1 😍</span>
+                  <span style="color:#f0ad4e">Yong-Jiu Grocery Store 😄</span>
+                  </p>
+               <p>
+                  <strong> Z </strong>
+                  <br>
+               </p>
+              </div>
+            </article>
+          </di>
+        </div>
+      </template>
+    </main-section>
+  </div>
+</template>
