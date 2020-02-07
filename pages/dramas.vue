f522d6ace583eb291d14be6b60c01f70b1278a1b
Emilie Bernard <56219642+Emilie-Bernard@users.noreply.github.com>
2020-02-07 10:29:24 +0100

    Change thailande color button

--- pages/dramas.vue
+++ pages/dramas.vue
@@ -12,7 +12,7 @@
             <div class="tile is-parent is"><button class="button is-primary" style="font-size:30px;">Corée</button></div>
             <div class="tile is-parent is"><button class="button is-warning" style="font-size:30px;">Taiwan</button></div>
             <div class="tile is-parent is"><button class="button is-japon" style="font-size:30px;">Japon</button></div>
-            <div class="tile is-parent is"><button class="button is-danger" style="font-size:30px;">Thailande</button></div>
+            <div class="tile is-parent is"><button class="button is-thailande" style="font-size:30px;">Thailande</button></div>
            
         </div>
         <!-- Posts in Category -->
