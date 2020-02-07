2e013ffa19e4f7f270409762e46f26159dd6aba7
Emilie <emilie1.bernard@gmail.com>
2020-02-07 19:24:26 +0100

    drama page

---
+++ pages/musiques.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="musiques-page" class="page-wrapper musiques-page">
+    <site-hero
+      title="Musiques"
+      subtitle="Des milliers d'artistes taiwanais à encore découvrir"
+      image="/uploads/drama-hero.jpg"
+    ></site-hero>
+    <main-section theme="one-column">
+    </main-section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Musiques',
+  head() {
+    return {
+      title: `Musiques | ${this.$siteConfig.siteName}`
+    }
+  }
+}
+</script>
