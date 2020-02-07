de58b9782b5d92897eee2572bcb4c0a1c7452787
Emilie Bernard <56219642+Emilie-Bernard@users.noreply.github.com>
2020-02-07 16:07:31 +0100

    Create thailande.vue

---
+++ pages/thailande.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="dramas-page" class="page-wrapper dramas-page">
+    <site-hero
+      title="Dramas Taiwanais"
+      subtitle="Pastel et simplicité"
+      image="/uploads/drama-hero.jpg"
+    ></site-hero>
+    <main-section theme="one-column">
+      <template v-slot:default> 
+      <div class="tile is-ancestor">
+            <div class="tile is-parent is"><router-link to='/chine'><button class="button is-danger" style="font-size:30px;">Chine</button></router-link></div>
+            <div class="tile is-parent is"><router-link to='/coree'><button class="button is-primary" style="font-size:30px;">Corée</button></router-link></div>
+            <div class="tile is-parent is"><router-link to='/taiwan'><button class="button is-warning" style="font-size:30px;">Taiwan</button></router-link></div>
+            <div class="tile is-parent is"><router-link to='/japon'><button class="button is-japon" style="font-size:30px;">Japon</button></router-link></div>
+            <div class="tile is-parent is"><router-link to='/thailande'><button class="button is-thailande" style="font-size:30px;">Thailande</button></router-link></div>
+        </div>
+        <div class="tile is-ancestor">
+          <div class="tile is-parent">
+            <article class="tile is-child box">
+              <p class="title">
+                La liste des dramas taiwanais que j'ai eu l'occasion de visionner
+              </p>
+              <p class="subtitle">
+                Petit à petit nous sombrons dans le monde infini des dramas 
+              </p>
+              <div class="content">
+              <p>
+              ⭐ = drama que j'ai adoré
+              <br>
+              😍 = drama que j'ai aimé
+              <br>
+              😄 = drama qui m'ont plus
+              </p>
+                <p>
+                  <strong> 0-9 </strong>
+                  <br>
+                  <span style="color:#56dcd8">2 Moons The Series ⭐</span>
+                </p>
+                <p>
+                  <strong> A - B - C - D - E </strong>
+                  <br> 
+                </p>
+                <p>
+                  <strong> F </strong>
+                  <br>
+                  <span style="color:#56dcd8">Full House ⭐</span>
+               </p>
+               <p>
+                  <strong> G - H - I - J </strong>
+                  <br>
+               </p>
+               <p>
+                  <strong> K </strong>
+                  <br>
+                  <span style="color:#56dcd8">Kiss Me ⭐</span>
+                </p>
+               <p>
+                  <strong> L - M- N - O - P - Q - R - S - T - U - V - W - X - Y - Z </strong>
+                  <br>
+               </p>
+              </div>
+            </article>
+          </di>
+        </div>
+      </template>
+    </main-section>
+  </div>
+</template>
