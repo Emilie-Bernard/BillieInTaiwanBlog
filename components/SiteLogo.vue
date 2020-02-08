1382e8ab913391f07fed0010c64f25b0020504c2
Emilie <emilie1.bernard@gmail.com>
2020-02-08 10:10:17 +0100

    drama page

--- components/SiteLogo.vue
+++ components/SiteLogo.vue
@@ -2,19 +2,11 @@
   <div class="logo-wrapper">
     <div
       :class="{
-        'logo-inner': true,
-        blink: blinking
+        'logo-inner': true
       }"
     >
       <img
-        v-for="x in frames"
-        :key="x"
-        :src="`/logo-frames/logo-f${x - 1}.svg`"
-      />
-      <img
-        v-for="x in frames"
-        :key="`${x}-r`"
-        :src="`/logo-frames/logo-f${frames - x}.svg`"
+        :src="`/logo.jpg`"
       />
     </div>
   </div>
@@ -24,25 +16,12 @@ export default {
   name: 'SiteLogo',
   data() {
     return {
-      frames: 5,
-      currentFrame: 1,
-      blinking: true
+      currentFrame: 1
     }
-  },
-  mounted() {
-    setInterval(() => {
-      const rand = Math.ceil(Math.random() * 10)
-      this.blinking = rand % 2
-    }, 1000)
   }
 }
 </script>
 <style lang="scss">
-@keyframes logoBlink {
-  100% {
-    transform: translateY(-360px);
-  }
-}
 .logo-wrapper {
   position: relative;
   height: 50px;
@@ -56,9 +35,6 @@ export default {
       height: 40px;
       margin: 5px;
       max-height: initial;
-    }
-    &.blink {
-      animation: logoBlink 0.2s steps(8, end) alternate;
     }
   }
 }
