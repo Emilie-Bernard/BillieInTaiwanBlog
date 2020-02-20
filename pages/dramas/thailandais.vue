bf5e52faea33a7e21d1d59b061d6cfd07e6e561f
Emilie Bernard <56219642+Emilie-Bernard@users.noreply.github.com>
2020-02-20 13:17:29 +0100

    Update thailandais.vue

--- pages/dramas/thailandais.vue
+++ pages/dramas/thailandais.vue
@@ -13,7 +13,6 @@
             <div class="tile is-parent is"><router-link to='/dramas/coreens'><button class="button is-primary" style="font-size:30px;">Corée</button></router-link></div>
             <div class="tile is-parent is"><router-link to='/dramas/taiwanais'><button class="button is-warning" style="font-size:30px;">Taiwan</button></router-link></div>
             <div class="tile is-parent is"><router-link to='/dramas/japonais'><button class="button is-japon" style="font-size:30px;">Japon</button></router-link></div>
-            <div class="tile is-parent is"><router-link to='/dramas/thailandais'><button class="button is-thailande" style="font-size:30px;">Thailande</button></router-link></div>
         </div>
         <div class="tile is-ancestor">
           <div class="tile is-parent">
